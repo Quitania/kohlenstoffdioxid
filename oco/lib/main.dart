d08c8cad1444aa311a74483a98c2b2da560cde42
Remko Plantenga <sonata82@gmail.com>
2020-12-20 18:50:44 +0100

    Added Gauge

--- oco/lib/main.dart
+++ oco/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'dart:typed_data';
 import 'dart:async';
+import 'dart:math';
 import 'package:usb_serial/usb_serial.dart';
 import 'package:usb_serial/transaction.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 void main() => runApp(MyApp());
 
@@ -17,6 +20,8 @@ class _MyAppState extends State<MyApp> {
   List<Widget> _serialData = [];
   StreamSubscription<String> _subscription;
   Transaction<String> _transaction;
+  int _currentValue = 400;
+  var _rng = new NotSoRandom();
 
   Future<bool> _connectTo(device) async {
     _serialData.clear();
@@ -110,9 +115,129 @@ class _MyAppState extends State<MyApp> {
           body: Center(
               child: Column(children: <Widget>[
                 Text('Status: $_status\n'),
-                Text("Result Data", style: Theme.of(context).textTheme.title),
-                ..._serialData,
+                Text('${"CO\u2082"} level', style: Theme.of(context).textTheme.title),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      new SizedBox(height: 325.0, child: Stack(
+                        children: [
+                          GaugeChart.fromData(_currentValue),
+                          Center(child: Text('$_currentValue', style: Theme.of(context).textTheme.title,))
+                        ],
+                      )),
+                    ],
+                  ),
+                ),
+                kDebugMode ? RaisedButton(onPressed: () => {
+                  setState(() => {
+                    _currentValue = _rng.nextInt(2000)
+                  })
+                }, child: Text("Generate new test value"),) : null
               ])),
         ));
+  }
+}
+
+class GaugeChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  GaugeChart(this.seriesList, {this.animate});
+
+  factory GaugeChart.withSampleData() {
+    return new GaugeChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+  factory GaugeChart.fromData(int currentValue) {
+    return new GaugeChart(
+      _createData(currentValue),
+      animate: false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.PieChart(seriesList,
+        animate: animate,
+        // Configure the width of the pie slices to 45px. The remaining space in
+        // the chart will be left as a hole in the center. Adjust the start
+        // angle and the arc length of the pie so it resembles a gauge.
+        defaultRenderer: new charts.ArcRendererConfig(
+            arcWidth: 45, startAngle: 4 / 5 * pi, arcLength: 7 / 5 * pi));
+  }
+
+  static List<charts.Series<GaugeSegment, String>> _createSampleData() {
+    final data = [
+      new GaugeSegment('Low', 1875, charts.MaterialPalette.green.shadeDefault),
+      new GaugeSegment('Acceptable', 1875, charts.MaterialPalette.yellow.shadeDefault),
+      new GaugeSegment('High', 1250, charts.MaterialPalette.red.shadeDefault),
+    ];
+
+    return [
+      new charts.Series<GaugeSegment, String>(
+        id: 'Segments',
+        domainFn: (GaugeSegment segment, _) => segment.segment,
+        measureFn: (GaugeSegment segment, _) => segment.size,
+        colorFn: (GaugeSegment segment, _) => segment.color,
+        data: data,
+      )
+    ];
+  }
+
+  static List<charts.Series<GaugeSegment, String>> _createData(int currentValue) {
+    var data = [
+      {'min': 400, 'max': 799, 'size': 1875, 'colour': charts.MaterialPalette.green.shadeDefault, 'segment': 'Low'},
+      {'min': 800, 'max': 1399, 'size': 1875, 'colour': charts.MaterialPalette.yellow.shadeDefault, 'segment': 'Medium'},
+      {'min': 1400, 'max': 1900, 'size': 1250, 'colour': charts.MaterialPalette.red.shadeDefault, 'segment': 'High'}
+    ].map((e) {
+      if (currentValue < e['min']) {
+        return [new GaugeSegment(e['segment'], e['size'], charts.MaterialPalette.transparent)];
+      } else if (currentValue > e['max']) {
+        return [new GaugeSegment(e['segment'], e['size'], e['colour'])];
+      } else {
+        int range = (e['max'] as int) - e['min'];
+        int difference = currentValue - e['min'];
+        double multiplier = difference / range;
+        int colouredNrOfPixels = (multiplier * e['size']).round();
+        // partly coloured, rest transparent
+        return [new GaugeSegment(e['segment'], colouredNrOfPixels, e['colour']), new GaugeSegment((e['segment'] as String) + '2', (e['size'] as int) - colouredNrOfPixels, charts.MaterialPalette.transparent)];
+      }
+    }).expand((element) => element).toList();
+
+    return [
+      new charts.Series<GaugeSegment, String>(
+        id: 'Segments',
+        domainFn: (GaugeSegment segment, _) => segment.segment,
+        measureFn: (GaugeSegment segment, _) => segment.size,
+        colorFn: (GaugeSegment segment, _) => segment.color,
+        data: data,
+      )
+    ];
+  }
+}
+
+class GaugeSegment {
+  final String segment;
+  final int size;
+  final charts.Color color;
+
+  GaugeSegment(this.segment, this.size, this.color);
+}
+
+class NotSoRandom {
+  int pointer = 0;
+  List<int> list = [405, 525, 550, 575, 600, 650, 700, 800, 1400, 1900, 2000];
+
+  int nextInt(int ignored) {
+    pointer++;
+    if (pointer > list.length-1) {
+      pointer = 0;
+    }
+    return list[pointer];
   }
 }
